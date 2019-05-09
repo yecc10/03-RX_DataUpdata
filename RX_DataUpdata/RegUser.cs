4adcd6f4f69cd6ccd06f8880b59b882158a0cac3
叶朝成 <yecc@ahrxgy.com>
2019-05-09 15:45:43 +0800

    更新密码验证

--- RX_DataUpdata/RegUser.cs
+++ RX_DataUpdata/RegUser.cs
@@ -16,6 +16,17 @@ namespace RX_DataUpdata
 
         private void RegNewUser_Click(object sender, EventArgs e)
         {
+            if (NUserPassword.Text != NUserPassword.Text || NUserPassword.Text.Length<6)
+            {
+                MessageBox.Show("二次填写的密码不统一,或密码长度小于6位！");
+                return;
+            }
+            if (NUserName.Text==string.Empty || Phone.Text == string.Empty || Email.Text == string.Empty||Keshi.Text == string.Empty || ACC.Text == string.Empty)
+            {
+                MessageBox.Show("当前信息未填写完成！");
+                return;
+            }
+            
             var manager = new UserManager();
             var user = new ApplicationUser() { UserName = NUserName.Text, PhoneNumber = Phone.Text, Email = Email.Text, Keshi = Keshi.Text,QuanXian=ACC.Text };
             IdentityResult result = manager.Create(user, NUserPassword.Text);
@@ -23,13 +34,11 @@ namespace RX_DataUpdata
             {
                 MessageBox.Show("注册成功！用你最新的账户登录吧！");
                 this.Close();
-                //IdentityHelper.SignIn(manager, user, isPersistent: false);
-                //IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
-
             }
             else
             {
-                //ErrorMessage.Text = result.Errors.FirstOrDefault();
+                MessageBox.Show("注册失败！请联系管理员打开服务器电脑！");
+                return;
             }
 
         }
